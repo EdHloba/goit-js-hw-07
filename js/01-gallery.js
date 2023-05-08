170741f2201e6643332d99b87e320a4ed94dd91c
EdHloba <hloba.e.v@gmail.com>
2023-05-08 16:04:50 +0300

    finally

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryList.addEventListener('click', onImageClick);
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+       <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    })
+    .join('');  
+}
+
+function onImageClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">`);
+    instance.show();
+
+    galleryList.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    });
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+        `;
+    })
+    .join('');  
+}
+
